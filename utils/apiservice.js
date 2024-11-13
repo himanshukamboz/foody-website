7dc9af47760a0601114fc28006e70ecd831cdf4c
himanshukamboz <Himanshukamboj5095@gmail.com>
2024-11-13 12:38:05 +0530

    improve

--- utils/apiservice.js
+++ utils/apiservice.js
@@ -1,1 +1,1 @@
-export const Menu_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.960059122809971&lng=77.57337538383284"
+export const Menu_API="https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.960059122809971&lng=77.57337538383284"
